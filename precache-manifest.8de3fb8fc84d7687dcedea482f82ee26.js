e3ef1f7a0ea51d1610935c7cc97c7ff4cdd21537
JoseLRiano <joselriano@yahoo.com>
2020-06-14 17:37:36 -0400

    Updates

---
+++ precache-manifest.8de3fb8fc84d7687dcedea482f82ee26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d53c2a5b42cec4c58dbc21272c890c",
+    "url": "/GroceriesList/index.html"
+  },
+  {
+    "revision": "e747a8598801ad077411",
+    "url": "/GroceriesList/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b52104b6be4da9f5f111",
+    "url": "/GroceriesList/static/js/2.f309ae75.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/GroceriesList/static/js/2.f309ae75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e747a8598801ad077411",
+    "url": "/GroceriesList/static/js/main.b670885e.chunk.js"
+  },
+  {
+    "revision": "f3271db905c595b04fe3",
+    "url": "/GroceriesList/static/js/runtime-main.4e62e5f3.js"
+  }
+]);
